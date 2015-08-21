6af7eecf6f88b4a0f1e1814b8da47b3e912cb182
Stian Håklev <shaklev@gmail.com>
2015-08-21 00:08:06 -0400

    Adding Prelude docs

---
+++ prelude/dist/sidebar_items.js
@@ -0,0 +1,17 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "Prelude",
+"docs": ["ok/1"]
+},{"id": "Prelude.Atom",
+"docs": ["to_string/1"]
+},{"id": "Prelude.List",
+"docs": ["indexify/1"]
+},{"id": "Prelude.Map",
+"docs": ["append_list/3","atomify/1","deep_put/3","del_in/3","group_by/2","stringify/1","switch/1","to_atom/1"]
+},{"id": "Prelude.String",
+"docs": ["is_integer?/1","to_int/1"]
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
