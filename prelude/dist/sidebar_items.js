3f48432ca89e87489bc6254e32b7125392eec692
Stian Håklev <shaklev@gmail.com>
2015-08-25 07:02:50 -0400

    Updating to Prelude 0.0.2

--- prelude/dist/sidebar_items.js
+++ prelude/dist/sidebar_items.js
@@ -9,7 +9,7 @@ sidebarNodes = {
 },{"id": "Prelude.Map",
 "docs": ["append_list/3","atomify/1","deep_put/3","del_in/3","group_by/2","stringify/1","switch/1","to_atom/1"]
 },{"id": "Prelude.String",
-"docs": ["is_integer?/1","to_int/1"]
+"docs": ["head/1","is_integer?/1","tail/1","to_int/1"]
 
   }]
 
